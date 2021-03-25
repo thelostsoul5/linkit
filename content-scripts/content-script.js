e8259779b241e3ad84d4ae0a376fb2a8622e2697
thelostsoul <1095533751@qq.com>
2021-03-25 10:19:48 +0800

    方案变更

--- content-scripts/content-script.js
+++ content-scripts/content-script.js
@@ -1,30 +1,10 @@
 (function () {
-  document.onmousedown = function (e) {
-    if (!e) {
-      e = window.event;
-    }
-    if (e.ctrlKey) {
-      whichElement(e);
-    }
-  };
+  document.querySelectorAll("a").forEach(function (item) {
+    item.onclick = function (event) {
+      if (event.ctrlKey) {
+        event.preventDefault();
+        console.log(item.href);
+      }
+    };
+  });
 })();
-
-function whichElement(e) {
-  var targ;
-  if (!e) {
-    var e = window.event;
-  }
-  if (e.target) {
-    targ = e.target;
-  } else if (e.srcElement) {
-    targ = e.srcElement;
-  }
-  if (targ.nodeType == 3) {
-    // defeat Safari bug
-    targ = targ.parentNode;
-  }
-  var tname = targ.tagName;
-  if ("A" == tname) {
-    alert(targ.href);
-  }
-}
